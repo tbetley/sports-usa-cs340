b48c068e7fd137b2e3f302b38fd61335350b9322
Tyler Betley <tyler.r.betley@gmail.com>
2020-03-02 22:10:09 -0600

    Added add item functionality, create items page

--- app.js
+++ app.js
@@ -11,10 +11,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+//const mysql = require("./dbcon-dev.js"); // CHANGE TO ./dbcon.js IN PRODUCTION
 const mysql = require("./dbcon.js");
 
-// Tell express to use public folder to serve assets
+
+/*************************
+** MIDDLEWARE
+*************************/
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 // local port
@@ -24,25 +30,36 @@ const PORT = process.env.PORT || 5000;
  * ROUTES
 ************************************/
 app.get("/", function (req, res) {
-    res.render("home.ejs", {title: "Sports USA"});  // title is the Tab Name, passed to ejs header
-})
+    res.render("home.ejs", {title: "Sports USA"});  
+});
 
 app.get("/cart", function(req, res) {
     res.render("cart.ejs", {title: "Sports USA - Cart"});
-})
+});
 
 app.get("/categories/:item", function(req, res) {
     var item = req.params.item;
     res.render("categories.ejs", {title: item})
-})
+});
 
 app.get("/signUp", function(req, res) {
     res.render("signUp.ejs", {title: "Sports USA - Sign Up"});
-})
+});
 
-app.get("/vendors", function (req, res) {
-    res.render("vendors.ejs", {title: "Sports USA - Vendors"});
-})
+app.get("/items", function (req, res) {
+    let callbackCount = 0;
+    let context = {};
+    context.title = "Sports USA - Items";
+
+    // query all items, send as object to template
+    getItems(context, mysql, complete);
+    function complete() {
+        callbackCount++;
+        if (callbackCount === 1) {
+            res.render("items.ejs", context)
+        }
+    }
+});
 
 app.get("/admin", function(req, res) {
     let callbackCount = 0;
@@ -59,7 +76,44 @@ app.get("/admin", function(req, res) {
             res.render("admin.ejs", context);
         }
     }
-})
+});
+
+app.post("/addItem", function(req, res) {
+    console.log(req.body);
+    let context = {};
+    context.title = "Add Item";
+    let callbackCount = 0;
+
+    addItem(req.body, mysql, complete);
+    function complete() {
+        callbackCount++;
+        if (callbackCount === 1)
+        {
+            res.render("addItem.ejs", context);
+        }
+    }
+});
+
+app.get("/addItem", function(req, res) {
+    let context = {};
+    context.title = "Add Item";
+    res.render("addItem.ejs", context);
+});
+
+
+// ERROR middleware
+app.use(function(req, res) {
+    res.type("text/plain");
+    res.status(404);
+    res.send("404 - Not Found");
+});
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+    res.type("plain/text");
+    res.status(500);
+    res.send("500 - Server Error");
+});
 
 
 app.listen(PORT, function() {
@@ -110,3 +164,16 @@ function getVendors(context, mysql, complete)
     });
 } 
 
+
+function addItem(context, mysql, complete)
+{
+    let query = `INSERT INTO items (vendorID, itemName, price, quantity, type, sport) VALUES ((SELECT vendorID FROM vendors WHERE vendorName="${context.vendor}"), "${context.itemName}", "${context.price}", ${context.quantity}, "${context.type}", "${context.sport}")`;
+
+    mysql.pool.query(query, function(err, results, fields) {
+        if (err) {
+            console.log(err);
+            complete();
+        }
+        complete();
+    });
+}
--- init.sql
+++ init.sql
@@ -1,3 +1,15 @@
+-- Sports USA Data Tables
+
+-- Set key checks and drop tables
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS vendors;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS orderItems;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Create Tables
 CREATE TABLE customers (
     customerID int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
@@ -51,16 +63,42 @@ CREATE TABLE orderItems (
     CONSTRAINT PK_orderItem PRIMARY KEY (orderNumber, itemID)
 );
 
+--Add New Item
+INSERT INTO items (vendorID, itemName, price, quantity, type, sport) VALUES (:vendorInput, :itemInput, :priceInput, :quantityInput, :typeInput, :sportInput);
+​
+--Add New Vendor
+INSERT INTO vendors (vendorName, website, phoneNumber) VALUES :vendorNameInput, :websiteInput, :numberInput);
+​
+--Add New Customer
+INSERT INTO customers (email, phoneNumber, logIn, firstName, lastName, streetAddress, city, zip, state) 
+VALUES (:emailInput, :numberInput, :logInput, :fnameInput, :lastNameInput, :addressInput, :cityInput, :zipInput, :stateInput);
+
+--Delete an Item
+Delete FROM items WHERE itemId = :itemDeletionSelection;
+​
+--Delete a Vendor
+Delete FROM vendors WHERE vendorID = :vendorDeletionSelection;
+​
+--Delete a Customer
+Delete FROM customers WHERE customerID = :customerDeletionSelection;
+​
+--Update a Customer
+UPDATE customers SET email = :emailInput, phoneNumber= :numberInput, logIn = :logInput, firstName= :fnameInput, lastName = :lastNameInput, streetAddress = :addressInput, city = :cityInput, zip = :zipInput, state = :stateInput WHERE id= :customer_ID_from_the_update_form;
+​
+--Update a Vendor
+UPDATE vendors SET vendorName = :vendorNameInput, website = :websiteInput, phoneNumber= :numberInput) WHERE vendorID = vendorID_selected_from_form;
+
+--Update an Item
+UPDATE items SET vendorID = :vendorInput, itemName = :itemInput, price=:priceInput, quantity = :quantityInput, type = :typeInput, sport = :sportInput WHERE itemId = item_ID_selected_from;
+​
 INSERT INTO customers (email, phoneNumber, logIn, firstName, lastName, streetAddress, city, zip, state) 
 VALUES ("bob@bob.com", "685-489-2343", "bob473", "Bob", "Bobero", "123 State Street", "Manhattan", 44345, "New York");
-
+​
 INSERT INTO customers (email, phoneNumber, logIn, firstName, lastName, streetAddress, city, zip, state) 
 VALUES ("Kevin@Kevin.com", "685-489-2343", "Kevin3224", "Kevin", "Kevinero", "432 State Street", "Manhattan", 74355, "Kansas");
-
+​
 INSERT INTO vendors (vendorName, website, phoneNumber) VALUES ("Nike", "nike.com", "390-999-0000");
-
+​
 INSERT INTO vendors (vendorName, website, phoneNumber) VALUES ("Champion", "champ.com", "900-939-0300");
-
-INSERT INTO items (vendorID, itemName, price, quantity, type, sport) VALUES ((SELECT vendorID FROM vendors WHERE vendorName="Nike"), "Packers T-Shirt", "$98.38", 25, "Apparel", "Football");
 
 INSERT INTO items (vendorID, itemName, price, quantity, type, sport) VALUES ((SELECT vendorID FROM vendors WHERE vendorName="Nike"), "Patriots T-Shirt", "$48.38", 19, "Apparel", "Football");
