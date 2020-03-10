b2ffb79736ef480fea7313780fc9d1d4878846e6
Tyler Betley <tyler.r.betley@gmail.com>
2020-03-09 22:26:44 -0500

    Fix env

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 // local port
-const PORT = process.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 /***********************************
  * ROUTES
